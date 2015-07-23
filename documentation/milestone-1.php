a4eeae381724814dfa27dfa86363ed94f246cb15
Humberto Perez <ferdinero121312@gmail.com>
2015-07-23 01:27:52 -0700

    added .gitignore and starting milestone-1 page

--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -1,35 +1,45 @@
 <!DOCTYPE html>
 	<html>
-			<title>  milestone-1</title>
+			<title>  Milestone-1</title>
            <link href="style.css"rel="stylesheet  " type=""text/css">
-			<head> Mission Statement</head>
-					<p> We want provide the best service to our customer. We want to help to satisfy their needs.</p>
-	            <p> following that spirit we want that any  person or businees , large or small  get the best  </p>
-					<p> results and getting big profits for theirs investment.</p>
-	<img src="documentation/pictures.jpg." alt pictures
+			<head> <h1>Mission Statement</h1> </head>
+					<p> We want to provide the best service to our customer. We want to help to satisfy their needs.</p>
+	            <p> Following that spirit, we want that any  person or businees , large or small,  get the best service.  </p>
+					<p> We always  will work to get the maximum return for your investment </p>
+
+
+
 	      <body>
 				<div clas="content ">
-					<div clas="header">gTitle</div>
+					<div clas="header">  <h2> Specific purpose</h2></div>
 					<div class="sidebar">
 
 
-					&raquo; Home
-						</br>
 
+					&raquo;Create a website for an existing business
 					</div>
 					<div class="main-content">
-						you write here
-						&nbsp
+						<h3> Motivation</h3>
+						&nbsp;
+						<img src="pictures.jpc"
 
 				</div>
-				<h1> GOAL</h1>
-				<p> Create a web site for a existent business so that business increase profits.</p>
-				<p>  The businees has twenty years but now they want to go online</p>
-				<h1> Persona</h1>
-				<p> The business  name is Arriba Mexico. It have been in Albuquerque. N.M for twenty years with out online operation</p>
-				<p> but the change on technology  make them to go online . They sell tradicional mexican Products,</p>
-				<p> Also they offer finantial services and they offer cell phones too.</p>
-				<h>AUDIENCE</h>
 
+				<>
+				<p>  The businees has been operating tradicionally  but now they want to go online</p>
+				<h1> Persona</h1>
+				<p> The business  name is Arriba Mexico. It have been in Albuquerque. N.M for twenty years without online operation</p>
+				<p> But the change in technology  make them to go online . They sell tradicional mexican Products,</p>
+				<p> Also they offer finantial services and they offer cell phones too.</p>
+				<h2>AUDIENCE</h2>
+				<p> Latino  Population, Hispanic. Mexican Inmigrant</p>
+					<p>, young populaton that wants good phones with no contract,</p>
+					<p> peers and profesinals that they want more businnes in a fast growing sector</p>
+					<h1> Specific Goals of the PWP </h1>
+					<li> Serve as example of having a website for a traditional business and getting future customers that
+						they want contrat ypur services.</li>
+					<li> Increase sales for the businees that contracted your service</li>
+					<li> Put customers in the web, so they can brand their busines a ours as web developers</li>
+					<li> help the community reaching  global markets </li>
 			</body>
   </html>
--- milestono1a.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html
-
-	<html>
-			<body>esta es un prueba</body>
-         <img src="documentation" alt="foto"
-	<html/>.
