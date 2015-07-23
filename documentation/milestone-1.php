5d110f9cf2076cd46e5fd5deba5a090a839226d8
Humberto Perez <ferdinero121312@gmail.com>
2015-07-22 22:40:57 -0700

    added .gitignore and starting milestone-1 page

--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -1,13 +1,27 @@
 <!DOCTYPE html>
 	<html>
 			<title>  milestone-1</title>
-
+           <link href="style.css"rel="stylesheet  " type=""text/css">
 			<head> Mission Statement</head>
 					<p> We want provide the best service to our customer. We want to help to satisfy their needs.</p>
 	            <p> following that spirit we want that any  person or businees , large or small  get the best  </p>
 					<p> results and getting big profits for theirs investment.</p>
 
 	      <body>
+				<div clas="content ">
+					<div clas="header">gTitle</div>
+					<div class="sidebar">
+
+
+					&raquo; Home
+						</br>
+
+					</div>
+					<div class="main-content">
+						you write here
+						&nbsp
+					<img src="documentation/pictures.jpg" width="330"/>
+				</div>
 				<h1> GOAL</h1>
 				<p> Create a web site for a existent business so that business increase profits.</p>
 				<p>  The businees has twenty years but now they want to go online</p>
