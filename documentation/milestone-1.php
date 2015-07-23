d5c9c7424b872ac9d6946b82a9777779378aeb90
Humberto Perez <ferdinero121312@gmail.com>
2015-07-22 19:42:24 -0700

    added .gitignore and starting milestone-1 page

---
+++ documentation/milestone-1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+	<html>
+			<title>  milestone-1</title>
+
+			<head> Mission Statement</head>
+					<p> We want provide the best service to our customer. We want to help to satisfy their needs.</p>
+	            <p> following that spirit we want that any  person or businees , large or small  get the best  </p>
+					<p> results and getting big profits for theirs investment.</p>
+
+	      <body>
+				<h1> GOAL</h1>
+				<p> Create a web site for a existent business so that business increase profits.</p>
+				<p>  The businees has twenty years but now they want to go online</p>
+				<h1> Persona</h1>
+				<p> The business  name is Arriba Mexico. It have been in Albuquerque. N.M for twenty years with out online operation</p>
+				<p> but the change on technology  make them to go online . They sell tradicional mexican Products,</p>
+				<p> Also they offer finantial services and they offer cell phones too.</p>
+				<h>AUDIENCE</h>
+
+			</body>
+  </html>
